bfb691c5a94711f54f1fafd6e1590f6dd1e031d9
KimHyeockJin <gurwls9628@naver.com>
2018-07-30 16:21:28 +0900

    src: 오늘의 문제 풀이
    문제 풀이

---
+++ src/Algo2439.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algo2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i = 0; i < n; ++i) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = i+1; j < n; ++j) {
+                sb.append(" ");
+            }
+            for (int j = 0; j <= i; ++j) {
+                sb.append("*");
+            }
+            System.out.println(sb.toString());
+        }
+    }
+}
