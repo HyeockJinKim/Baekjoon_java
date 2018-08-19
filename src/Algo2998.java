4dfd360c5c3861be82af926ddce54710922935f2
KimHyeockJin <gurwls9628@naver.com>
2018-08-19 23:39:52 +0900

    src: 문제 풀이
    하루 문제 풀이

--- src/Algo2998.java
+++ src/Algo2998.java
@@ -7,16 +7,18 @@ public class Algo2998 {
         char[] bin = br.readLine().toCharArray();
         br.close();
         int mod = bin.length%3;
-        for (int i = 0; i < mod; ++i) {
-
+        int sum = 0, dub = 1;
+        for (int i = mod-1; i >= 0; --i) {
+            sum += dub*(bin[i]-'0');
+            dub *= 2;
         }
-        for (int i = bin.length-3; i >= mod; i -= 3) {
-            int sum = (bin[i]-'0')*4+(bin[i+1]-'0')*2+(bin[i+2]-'0');
-
-            bw.write();
+        if (sum != 0) {
+            bw.write(String.valueOf(sum));
         }
-
-
+        for (int i = mod; i <= bin.length-3; i += 3) {
+            sum = (bin[i]-'0')*4+(bin[i+1]-'0')*2+(bin[i+2]-'0');
+            bw.write(String.valueOf(sum));
+        }
         bw.close();
     }
 }
