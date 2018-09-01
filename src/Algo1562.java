1919ab7f04f9123163cf2f4fe5e36b1cf781ee38
KimHyeockJin <gurwls9628@naver.com>
2018-09-01 23:58:45 +0900

    src: 오늘의 문제풀이
    문제 풀이

---
+++ src/Algo1562.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Algo1562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        int[][] dp = new int[n+1][10];
+        if (n >= 10)
+            dp[10][9] = 1;
+        for (int i = 11; i <= n; ++i) {
+            dp[i][0] = dp[i-1][1];
+            dp[i][9] = dp[i-1][8];
+            for (int j = 1; j < 9; ++j) {
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1];
+            }
+        }
+        int num = 0;
+        for (int i = 0; i <= 9; ++i) {
+            num += dp[n][i];
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(num));
+        bw.close();
+    }
+}
