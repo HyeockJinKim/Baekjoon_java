953ff592ab2ea8119761fbc16bdb47f23c6ce6ff
KimHyeockJin <gurwls9628@naver.com>
2018-10-13 21:35:58 +0900

    src: 문제풀이

---
+++ src/Algo11722.java
@@ -0,0 +1,26 @@
+//import java.io.*;
+//import java.util.StringTokenizer;
+//
+//public class Algo11722 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+//        int[] a = new int[n+1];
+//        for (int i = 1; i <= n; ++i) {
+//            a[i] = Integer.parseInt(tokenizer.nextToken());
+//        }
+//        br.close();
+//        int[][] dp = new int[n][n];
+//        for (int i = 1; i <= n; ++i) {
+//            for (int j = i; j <= n; ++j) {
+//                if (a[i] )
+//                dp[i][j]
+//            }
+//        }
+//
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        bw.close();
+//    }
+//}
